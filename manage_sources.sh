937c0bd9f63c5ca2d0cdeb823fbfff83b60332bd
Emir Cem Gezer <emircem.gezer@gmail.com>
2021-11-19 11:14:48 -0500

    chore: add licenses and change dvl package name

--- manage_sources.sh
+++ manage_sources.sh
@@ -4,6 +4,7 @@ ALL_SOURCE_DIRS=`find . -name "package.xml" -exec dirname {} \;`
 
 SIMULATION_DIRS="
 alpha_controller
+alpha_localization
 alpha_description
 alpha_teleop
 alpha_simulator
@@ -15,14 +16,14 @@ function setup_simulation() {
     for d in ${SIMULATION_DIRS[@]} ; do
       echo $src | grep $d -q
       if [ $? -ne 0 ] ; then
-        touch $src/CATKIN_IGNORE 
+        touch $src/CATKIN_IGNORE
       fi
     done
   done
 }
 
 function setup_vehicle() {
-  find . -name "CATKIN_IGNORE" -exec rm {} \; 
+  find . -name "CATKIN_IGNORE" -exec rm {} \;
 }
 
 $1
